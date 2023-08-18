6b5b07758023d4301028ab604fc7f8310e951d66
ltk2801 <luutuankhanha3dt@gmail.com>
2023-08-18 17:45:31 +0700

    complete UI toursPage & tourDetailPage

--- frontend/src/router/Routers.js
+++ frontend/src/router/Routers.js
@@ -13,7 +13,7 @@ const Routers = () => {
       <Route path="/" element={<Navigate to="/home" />} />
       <Route path="/home" element={<Home />} />
       <Route path="/tours" element={<Tours />} />
-      <Route path="/tour/:id" element={<TourDetails />} />
+      <Route path="/tours/:id" element={<TourDetails />} />
       <Route path="/login" element={<Login />} />
       <Route path="/register" element={<Register />} />
       <Route path="/tour/search" element={<SearchResultList />} />
